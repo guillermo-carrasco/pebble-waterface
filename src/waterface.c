298df8dee027b6d3969ab7c237c2ef0666bf1071
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-03-21 21:02:15 +0100

    Now it shows the time!

--- src/waterface.c
+++ src/waterface.c
@@ -4,15 +4,16 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 
 
+/********** WINDOW MANAGEMENT **********/
+
 static void main_window_load(Window *window) {
     // Create time TextLayer
     s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
-    text_layer_set_text(s_time_layer, "Tja tja Guillem! ^^");
 
     // Improve the layout to be more like a watchface
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
     text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
     // Add it as a child layer to the Window's root layer
@@ -23,6 +24,35 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+}
+
+
+/********** APP CONTENT **********/
+
+static void update_time() {
+    // Get a tm structure
+    time_t temp = time(NULL);
+    struct tm *tick_time = localtime(&temp);
+
+    // Create a long-lived buffer
+    static char buffer[] = "00:00";
+
+    // Write the current hours and minutes into the buffer
+    if(clock_is_24h_style() == true) {
+        // Use 24 hour format
+        strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    } else {
+        // Use 12 hour format
+        strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    }
+
+    // Display this time on the TextLayer
+    text_layer_set_text(s_time_layer, buffer);
+}
+
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
 }
 
 
@@ -38,6 +68,13 @@ static void init() {
 
     // Show the Window on the watch, with animated=true
     window_stack_push(s_main_window, true);
+
+    // Register with TickTimerService: This method will be called everytime that
+    // the specified time unit changes, minutes in this case
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+    // Make sure the time is displayed from the start
+    update_time();
 }
 
 
